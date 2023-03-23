0063f8e4fe9fea2ba8f862785b5092a600794912
kenta asakura <asakura.kenta963420@gmail.com>
2023-03-23 17:00:25 +0900

    projects section slide in javascript added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -17,4 +17,46 @@ window.addEventListener('scroll', () => {
 
   // resets scroll value
   lastScrollY = window.scrollY;
+
+  // function projects_slidein() {
+  //   console.log(window.innerHeight);
+  // }
+
+  // projects_slidein();
 });
+
+
+
+window.addEventListener('scroll', () => {
+  projectLeftSlideIn();
+});
+
+function projectLeftSlideIn() {
+  const projectsLeft = document.querySelectorAll('.project-left')
+  const projectsRight = document.querySelectorAll('.project-right')
+  const triggerBottom = window.innerHeight / 5 * 4;
+
+  console.log(triggerBottom);
+
+  projectsLeft.forEach((project) => {
+    const projectLeftTop = project.getBoundingClientRect().top;
+
+    console.log(projectLeftTop);
+    if (projectLeftTop < triggerBottom) {
+      project.classList.add('show');
+    } else {
+      project.classList.remove('show')
+    }
+  });
+
+  projectsRight.forEach((project) => {
+    const projectRightTop = project.getBoundingClientRect().top;
+
+    console.log(projectRightTop);
+    if (projectRightTop < triggerBottom) {
+      project.classList.add('show');
+    } else {
+      project.classList.remove('show')
+    }
+  });
+}
