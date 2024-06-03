ccea035ebaf5f9448c99c301c64cbeae5cecc7f2
kenta asakura <asakura.kenta963420@gmail.com>
2024-06-03 13:43:06 +0900

    contact section HTML and styling (SCSS) refactored

--- source/javascripts/contactFormPreventDefault.js
+++ source/javascripts/contactFormPreventDefault.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function() {
-  const form = document.getElementById('form');
+  const form = document.getElementsByClassName('form');
+
   form.addEventListener("submit", function(e) {
     e.preventDefault();
     const data = new FormData(form);
