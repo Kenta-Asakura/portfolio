d3526fcabe463f06f00edb31fb66a8ac0d63a064
kenta asakura <asakura.kenta963420@gmail.com>
2024-06-09 11:45:20 +0900

    config update for deployment

--- config.rb
+++ config.rb
@@ -40,7 +40,18 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+  # configure :build do
+  #   activate :minify_css
+  #   activate :minify_javascript
+  # end
+
+  configure :build do
+    activate :minify_css
+    activate :minify_javascript
+
+    # Append a hash to asset urls (make sure to use the url helpers)
+    activate :asset_hash
+
+    activate :asset_host, :host => '//kentaasakura.pro.cloudfront.net'
+  end
+    
